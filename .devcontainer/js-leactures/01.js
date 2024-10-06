d3962976ebc7817d23e15de3d2d8f9ebde8f2092
ABHISHEK KUMAR <168332980+Abhi2602k@users.noreply.github.com>
2024-10-06 19:45:09 +0000

    leacture 5 complete

--- .devcontainer/js-leactures/01.js
+++ .devcontainer/js-leactures/01.js
@@ -1,1 +1,16 @@
-console.log("abhishek");
+// const a=10;  ab a ki value chnage nhi ho skti agar hum a ko update bhi krde or same variabe ke liye baar baar declare krne ki jarurat bhi nhi hoti
+const a=10;
+// a=5;
+ console.log(a);
+ // var ko let ki value updare ho skti h or console table ka use ek se jada output ko eksath table ki form me print kra skte h
+ var b="123"
+ let c="igigoo"
+ var b=100
+ c=4654
+ let d;
+ console.table([a,b,c,d]);
+ // agar koi veriable ki value assign nhi kroge to js use undefined hi manta h
+ // var ka use morden js me nhi hota wo scope{} me probblem krta tha mtlb same variable ko hum baar baar declare kr skte the jitni baar declare krte wo value update ho jati thi lekin let or const ke sath esa nhi h tum ek baar hi ek variable ko assign kr skte ho
+ let e=100
+ f=909
+
