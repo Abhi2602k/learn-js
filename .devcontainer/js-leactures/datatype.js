db3dfd6e2bd39fb72a10d8c7ece722da857f8cd8
ABHISHEK KUMAR <168332980+Abhi2602k@users.noreply.github.com>
2024-10-07 14:00:02 +0000

    089

--- .devcontainer/js-leactures/datatype.js
+++ .devcontainer/js-leactures/datatype.js
@@ -2,4 +2,4 @@
 // premitive datatype=string, boolean,number,null undefined
  // non premitive=arrays, object , funcation
   // js ek dynamic language h kyoki isme hum variable ko use krne ke baad use define kr ste h or language c++ inme pehle declare krna padta h
-  
+  let arr=[10,56,30]
